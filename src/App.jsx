7de39c5b1739a647ba7065937f1a19122add67c4
rafael1420 <rafael.gomez2007@gmail.com>
2025-08-22 22:00:51 -0300

    Css e codificação das funcionalidades do game

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { QuizContext } from './context/quiz';
 
 import Welcome from './components/Welcome';
 import Question from './components/Question';
+import GameOver from './components/GameOver';
 
 import './App.css';
-
 
 
 function App() {
@@ -22,6 +22,7 @@ function App() {
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
 
     </div>
     </>
